981b705ce60d484bb207e1dae1b9c8209e78cd21
CarlosMonforteIzquierdo <carlosmonforteizquierdo@gmail.com>
2024-12-21 16:37:14 +0100

    First commit

--- get_wallet_token_holdings.py
+++ get_wallet_token_holdings.py
@@ -1,4 +1,4 @@
-from theblockchainapi import SolanaAPIResource, SolanaNetwork,SolanaCurrencyUnit
+from theblockchainapi import SolanaAPIResource, SolanaNetwork,SolanaCurrencyUnit,BlockchainAPIResource,Blockchain,BlockchainNetwork
 from decouple import config
 
 API_KEY = config("BLOCKCHAIN_API_KEY")
@@ -10,7 +10,13 @@ BLOCKCHAIN_API_RESOURCE = SolanaAPIResource(
     api_key_id=API_KEY,
     api_secret_key=SECRET_KEY
 )
-wallet_address ="Eo44seMeJqJ6LtrAvvP5oYMQjvYtBAV1J5s3kzZqeGDz"
+
+SOLANA_BLOCKCHAIN_API_RESOURCE = BlockchainAPIResource(
+    api_key_id=API_KEY,
+    api_secret_key=SECRET_KEY,
+    blockchain=Blockchain.SOLANA,
+    network=BlockchainNetwork.SolanaNetwork.MAINNET_BETA
+)
 
 def get_token_holdings(wallet_address):
     return BLOCKCHAIN_API_RESOURCE.get_wallet_token_holdings(
@@ -20,9 +26,29 @@ def get_token_holdings(wallet_address):
         network=DEFAULT_NETWORK
     )
 
+def get_token_name(mint_address):
+    token_info = BLOCKCHAIN_API_RESOURCE.get_token_metadata(
+        mint_address=mint_address,
+        network=DEFAULT_NETWORK
+    )
+    return token_info.get("name", "Unknown Token")
+
 def main():
+    wallet_address ="Eo44seMeJqJ6LtrAvvP5oYMQjvYtBAV1J5s3kzZqeGDz"
     token_holdings = get_token_holdings(wallet_address)
-    print(f"Tokens: {token_holdings}")
+    
+    for token in token_holdings:
+        mint_address = token["mint_address"]
+        ui_amount = token["ui_amount"]
+        token_name = get_token_name(mint_address)
+        #Falta conectar con API de CoinGecko para obtener el precio en USD
+        price_in_USD = 1.0
+        amount_in_usd = ui_amount * price_in_USD
+
+        print(f"Token Name: {token_name}")
+        print(f"Mint Address: {mint_address}")
+        print(f"UI Amount: {ui_amount}")
+        print(f"Amount (in USD): ${amount_in_usd:.2f}")
 
 
 
